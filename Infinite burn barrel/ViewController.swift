763b50af77482931f15e014a8fe2ca76d5a5eb55
Mohamed Hage <mohamedhage@macbook-air-2.local>
2018-02-27 02:34:29 -0500

    Initial Commit

---
+++ Infinite burn barrel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Infinite burn barrel
+//
+//  Created by Mohamed Hage on 2018-02-27.
+//  Copyright © 2018 Mohamed Hage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
