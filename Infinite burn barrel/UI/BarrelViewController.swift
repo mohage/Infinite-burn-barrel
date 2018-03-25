a93cda90574c1ccf64c40554f2e74b5b508ee976
Dejan Agostini <dejan.agostini@gmail.com>
2018-03-25 15:01:39 +0100

    Finishes UI for the barrel controller.

---
+++ Infinite burn barrel/UI/BarrelViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BarrelViewController.swift
+//  Infinite burn barrel
+//
+//  Created by Dejan on 25/03/2018.
+//  Copyright © 2018 Mohamed Hage. All rights reserved.
+//
+
+import UIKit
+
+class BarrelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
